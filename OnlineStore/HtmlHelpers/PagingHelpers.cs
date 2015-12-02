59875edd8863838b20a230fe976f965f1b1ec7fa
Vadim Teslya <TeslyaVadim2@gmail.com>
2015-12-02 14:44:10 +0200

    Add switching of pages

---
+++ OnlineStore/HtmlHelpers/PagingHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Web.Mvc;
+using OnlineStore.Models;
+
+namespace OnlineStore.HtmlHelpers
+{
+    public static class PagingHelpers
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper html , PageInfo pageInfo, Func<int, string> pageUrl)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 1; i <= pageInfo.TotalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a"); // Construct an <a> tag
+                tag.MergeAttribute("href", pageUrl(i));
+                tag.InnerHtml = i.ToString();
+                if (i == pageInfo.CurrentPage)
+                tag.AddCssClass("selected");
+                result.Append(tag.ToString());
+            }
+
+            return MvcHtmlString.Create(result.ToString());
+        }
+
+    }
+}
