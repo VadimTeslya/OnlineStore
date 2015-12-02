4bf7d36c0603e6ca30f576564fba0478eaab6929
Vadim Teslya <TeslyaVadim2@gmail.com>
2015-12-02 15:05:12 +0200

    Add AdminController

---
+++ OnlineStore/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using OnlineStoreData;
+using OnlineStoreEntity;
+
+namespace OnlineStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IRepository<Product> _productService;
+
+        public AdminController(IRepository<Product> product)
+        {
+            _productService = product;
+        }
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            return View(_productService.GetAll());
+        }
+
+    }
+}
