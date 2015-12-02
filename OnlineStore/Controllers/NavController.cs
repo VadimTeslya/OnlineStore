f0578dbfbb908e8b9ce6922c44cea5fcc4384c6e
Vadim Teslya <TeslyaVadim2@gmail.com>
2015-12-02 14:56:25 +0200

    Add menu panel

---
+++ OnlineStore/Controllers/NavController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using OnlineStoreData;
+using OnlineStoreEntity;
+
+namespace OnlineStore.Controllers
+{
+    public class NavController : Controller
+    {
+        private IRepository<Category> _categoryService;
+
+        public NavController(IRepository<Category> category)
+        {
+            _categoryService = category;
+        }
+        //
+        // GET: /Nav/
+
+        public PartialViewResult Menu(string category = null)
+        {
+            ViewBag.SelectedCategory = category;
+            var categories = _categoryService.GetAll()
+                .Select(c => c.Name)
+                .Distinct()
+                .OrderBy(c => c);
+
+            return PartialView(categories);
+        }
+    }
+}
