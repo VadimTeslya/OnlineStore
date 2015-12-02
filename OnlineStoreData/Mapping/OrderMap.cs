f2af7c12a21e1f25129d1e6123e507d3579579a6
Vadim Teslya <TeslyaVadim2@gmail.com>
2015-12-02 15:07:50 +0200

    Refactoring Order model

--- OnlineStoreData/Mapping/OrderMap.cs
+++ OnlineStoreData/Mapping/OrderMap.cs
@@ -10,6 +10,14 @@ namespace OnlineStoreData.Mapping
         {
             ToTable("Orders");
             HasKey(o => o.Id).Property(o => o.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            HasMany(o => o.Products)
+                .WithMany(p => p.Orders)
+                .Map(mc =>
+                {
+                    mc.ToTable("OrderJoinProduct");
+                    mc.MapLeftKey("OrderId");
+                    mc.MapRightKey("ProductId");
+                });
 
             Property(o => o.PhoneNumber).IsRequired();
             Property(o => o.CustomerName).IsRequired().HasMaxLength(100);
--- OnlineStoreEntity/Order.cs
+++ OnlineStoreEntity/Order.cs
@@ -1,15 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OnlineStoreEntity
 {
     public class Order: IEntity
     {
+        public Order()
+        {
+            Products = new List<Product>();
+        }
+
         public int Id { get; set; }
         public int PhoneNumber { get; set; }
         public string CustomerName { get; set; }
         public string DeliveryAddress { get; set; }
-        public virtual ShoppingСart ShoppingСart { get; set; }
         public virtual OrderStatus OrderStatus { get; set; }
         public DateTime OrderDate { get; set; }
+        public virtual ICollection<Product> Products { get; set; } 
     }
 }
