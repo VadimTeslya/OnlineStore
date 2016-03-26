f3f35f8861e51eba51bc1ded3ee91248f9b09168
VadimTeslya <TeslyaVadim2@gmail.com>
2016-03-26 14:18:40 +0200

    Moving CRUD opperation from ProductController to AdminController

--- OnlineStore/Controllers/ProductController.cs
+++ OnlineStore/Controllers/ProductController.cs
@@ -10,14 +10,11 @@ namespace OnlineStore.Controllers
     public class ProductController : Controller
     {
         private IRepository<Product> _productService;
-        private IRepository<Category> _categoryService;
+        private int PageSize = 12;
 
-        public int PageSize = 6;
-
-        public ProductController(IRepository<Product> productService, IRepository<Category> categoryService)
+        public ProductController(IRepository<Product> productService)
         {
             _productService = productService;
-            _categoryService = categoryService;
         }
 
         public ActionResult Index(string category, int page = 1)
@@ -45,103 +42,14 @@ namespace OnlineStore.Controllers
             return View(model);
         }
 
-        public ActionResult Details(int id)
+        public FileContentResult GetPhoto(int id)
         {
             var product = _productService.GetById(id);
-            
-            return View(product);
-        }
-
-        //
-        // GET: /Category/Create
-
-        public ActionResult Create()
-        {
-            var categoryList = _categoryService.GetAll().ToList();
-            ViewBag.Id = new SelectList(categoryList, "Id", "Name");
-
-            return View();
-        }
-
-        //
-        // POST: /Category/Create
-
-        [HttpPost]
-        public ActionResult Create(Product model)
-        {
-            if (string.IsNullOrEmpty(model.Name))
+            if (product != null)
             {
-                ModelState.AddModelError("Name", "Field name is required");
+                return File(product.ProductPhoto, product.PhotoMimeType);
             }
-
-            if (ModelState.IsValid)
-            {
-                _productService.Insert(model);
-
-                return RedirectToAction("Index");
-            }
-                
-            var categoryList = _categoryService.GetAll().ToList();
-            ViewBag.Id = new SelectList(categoryList, "Id", "Name");
-
-            return View(model);
-        }
-
-        //
-        // GET: /Category/Edit/5
-
-        public ActionResult Edit(int id)
-        {
-            var product = _productService.GetById(id);
-
-            return View(product);
-        }
-
-        //
-        // POST: /Category/Edit/5
-
-        [HttpPost]
-        public ActionResult Edit(Product model)
-        {
-            if (ModelState.IsValid)
-            {
-                _productService.Update(model);
-
-                return RedirectToAction("Index");
-            }
-                
-            return View(model);
-        }
-
-        //
-        // GET: /Category/Delete/5
-
-        public ActionResult Delete(int id)
-        {
-            var product = _productService.GetById(id);
-
-            return View(product);
-
-        }
-
-        //
-        // POST: /Category/Delete/5
-
-        [HttpPost]
-        public ActionResult Delete(int id, Product model)
-        {
-            try
-            {
-                var product = _productService.GetById(id);
-
-                _productService.Delete(product);
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            return null;
         }
     }
 }
